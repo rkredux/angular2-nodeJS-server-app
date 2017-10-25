21bddb8ca913cbbc7b176d6bb9ca01811c66e918
Rahul <rkredux@gmail.com>
2017-10-25 05:57:15 -0400

    Bootstraping Root Module

--- routes/app.js
+++ routes/app.js
@@ -1,34 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var User = require("../models/user"); 
 
 router.get('/', function (req, res, next) {
-	User.findOne({}, function(err, doc){
-		if(err){
-			return res.send("No such user exists"); 
-		} else{
-			return res.render("message", {email: doc.email})
-		}
-	})
+	res.render("index"); 
 });
 
-router.get("/message", function(req, res, next){
-	res.send("Email has been saved"); 
-})
-
-
-router.post("/", function(req, res, next){
-	const email = req.body.email;
-	const user = new User({
-		firstName: "Rahul", 
-		lastName: "Kumar", 
-		password: "secret", 
-		email: email
-	}); 
-
-	user.save(); 
-	console.log(user); 
-	res.redirect("/message"); 
-}); 
 
 module.exports = router;
