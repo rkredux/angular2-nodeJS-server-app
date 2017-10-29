a4138c61613b07ed04b7545d04149fdb8cc38411
Rahul <rkredux@gmail.com>
2017-10-29 06:29:08 -0400

    Set up Message component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,31 +1,12 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
     selector: 'my-app',
-
-    templateUrl: './app.component.html', 
-    
-    styles: [`
-    .author{
-    display: inline-block; 
-    font-style: italic; 
-    font-size: 12px; 
-    width: 80%; 
-    }
-
-    .config{
-    display: inline-block; 
-    text-align: right; 
-    font-size: 12px; 
-    width: 19%;
-    }
-
-    `]
+    templateUrl: './app.component.html',  
 })
 
 export class AppComponent {
-	message = {
-		content: "A message", 
-		author: "Rahul Kumar"
-	}
+	message: Message = new Message("Some message", "Max"); 
 }
